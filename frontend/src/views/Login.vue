6ecfc8c7a7c56bd86b17570788a3202f2489984c
Hussein Mohamed <hussen88gamer88@gmail.com>
2024-03-16 22:55:18 +0300

    Update Login.vue

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -1,42 +1,87 @@
 <template>
 	<ion-page>
 		<ion-content class="ion-padding">
-			<div class="flex h-screen w-screen flex-col justify-center bg-white">
-				<div class="flex flex-col mx-auto gap-3 items-center">
+			<div class="flex h-screen w-screen bg-white">
+				<div
+					class="flex flex-col gap-3 bg-[#A3312A] w-[55%] pl-24 justify-center"
+				>
 					<!-- <FrappeHRLogo class="h-8 w-8" /> -->
-					<img src="/LogoAlRaya.png" width="100"/>
-					<div class="text-3xl font-semibold text-red-600 text-center">
-						login to Al-Raiya HR
+					<div class="flex items-center gap-4">
+						<img src="/LogoAlRaya.png" class="w-16" />
+						<h2 class="text-white text-3xl font-bold tracking-wider">
+							AL'RAIYA GROUP
+						</h2>
 					</div>
+
+					<h1
+						class="text-white text-[54px] font-bold leading-[60px] tracking-wide"
+					>
+						Welcome To <br />
+						Al-Raiya Group
+					</h1>
+					<p class="text-white font-thin flex items-center">
+						Please Login To Use The System
+						<FeatherIcon name="chevron-right" class="h-6 w-6" />
+						<FeatherIcon name="chevron-right" class="h-6 w-6 -ml-4" />
+					</p>
 				</div>
 
-				<div class="mx-auto mt-10 w-full px-8 sm:w-96">
-					<form class="flex flex-col space-y-4" @submit.prevent="submit">
-						<Input
-							label="Email"
-							placeholder="johndoe@mail.com"
-							v-model="email"
-						/>
-						<!-- :type="email !== 'Administrator' ? 'email' : 'text'" -->
+				<div
+					class="w-[45%] bg-[#FEFBFB] h-full flex justify-center items-center"
+				>
+					<div>
+						<h1 class="font-bold text-3xl text-[#c24c4c] mb-4">Login</h1>
+						<form class="flex flex-col gap-4" @submit.prevent="submit">
+							<label for="email" class="-mb-3">Email</label>
+							<div
+								class="bg-white rounded-2xl border border-gray-100/95 h-10 relative overflow-hidden"
+							>
+								<FeatherIcon
+									name="mail"
+									class="h-5 w-5 left-1 top-1/2 z-10 -translate-y-[50%] text-[#c24c4c] absolute"
+								/>
+								<Input
+									placeholder="johndoe@mail.com"
+									v-model="email"
+									id="email"
+									class="border-none w-[400px] h-10 m-0 pl-4 bg-white px-0 outline-none focus:shadow-none focus:ring-0 hover:bg-white"
+								/>
+							</div>
 
-						<Input
-							label="Password"
-							type="password"
-							placeholder="••••••"
-							v-model="password"
-							variant="outline"
-						/>
-						<ErrorMessage :message="session.login.error" />
-						<Button
-							:loading="session.login.loading"
-							variant="solid"
-							theme="red"
-							:disabled="password=== null ?true :false"
-							class="disabled:bg-gray-700 disabled:text-white !mt-6"
-						>
-							Login
-						</Button>
-					</form>
+							<!-- :type="email !== 'Administrator' ? 'email' : 'text'" -->
+							<label for="password" class="mb-[-10px]">Password</label>
+							<div
+								class="bg-white rounded-2xl border border-gray-100/95 h-10 relative overflow-hidden"
+							>
+								<FeatherIcon
+									name="lock"
+									class="h-5 w-5 left-1 top-1/2 z-10 -translate-y-[50%] text-[#c24c4c] absolute"
+								/>
+								<Input
+									class="border-none w-full h-10 m-0 pl-4 bg-white px-0 outline-none focus:shadow-none focus:ring-0 hover:bg-white"
+									type="password"
+									id="password"
+									placeholder="••••••"
+									v-model="password"
+									variant="outline"
+								/>
+							</div>
+
+							<ErrorMessage :message="session.login.error" />
+							<Button
+								:loading="session.login.loading"
+								variant="solid"
+								theme="red"
+								:disabled="password === null ? true : false"
+								class="rounded-2xl text-white bg-[#c24c4c] cursor-pointer"
+							>
+								<div class="flex justify-between w-full items-center">
+									<p class="ml-auto w-full">Login</p>
+									<FeatherIcon name="chevron-right" class="h-6 w-6" />
+								</div>
+							</Button>
+						</form>
+					</div>
 				</div>
 			</div>
 		</ion-content>
@@ -46,7 +91,7 @@
 <script setup>
 import { IonPage, IonContent } from "@ionic/vue"
 import { inject, ref } from "vue"
-import { Input, Button, ErrorMessage } from "frappe-ui"
+import { Input, Button, ErrorMessage, FeatherIcon } from "frappe-ui"
 
 import FrappeHRLogo from "@/components/icons/FrappeHRLogo.vue"
 
